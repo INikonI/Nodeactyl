bcdd6abb1b9ed286d0d18a2190deef91455c3b16
Nikon <47792796+INikonI@users.noreply.github.com>
2021-04-11 00:02:26 +0500

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const Request = require('./ClientRequest.js');
 
 // GET
 const getallservers = require('./methods/getAllServers.js');
@@ -17,6 +18,84 @@ const killserver = require('./methods/killServer.js');
 const restartserver = require('./methods/restartServer.js');
 const sendcommand = require('./methods/sendCommand.js');
 
+// СУКА ПОЧЕМУ ТУТ ЭТОГО НЕ БЫЛО?!  by nikon
+class Client {
+	/**
+ 	*
+ 	* @param {String} HOST Host to use
+ 	* @param {String} KEY Client API key
+ 	*/
+	constructor(host, key) {
+		this.host = host
+		this.key = key
+	}
+	
+	getAllServers() {
+		const Req = new Request(this.host, this.key);
+		return Req.getRequest('GetAllServers', null);
+	}
+
+	getCPUCores(ServerID) {
+		const Req = new Request(this.host, this.key);
+		return Req.getRequest('GetCPUCores', ServerID);
+	}
+
+	getCPUUsage(ServerID) {
+		const Req = new Request(this.host, this.key);
+		return Req.getRequest('GetCPUUsage', ServerID);
+	}
+
+	getDiskUsage(ServerID) {
+		const Req = new Request(this.host, this.key);
+		return Req.getRequest('GetDiskUsage', ServerID);
+	}
+
+	getRAMUsage(ServerID) {
+		const Req = new Request(this.host, this.key);
+		return Req.getRequest('GetRAMUsage', ServerID);
+	}
+
+	getServerInfo(ServerID) {
+		const Req = new Request(this.host, this.key);
+		return Req.getRequest('GetServerInfo', ServerID);
+	}
+
+	isOwner(ServerID) {
+		const Req = new Request(this.host, this.key);
+		return Req.getRequest('IsOwner', ServerID);
+	}
+
+	killServer(ServerID) {
+		const Req = new Request(this.host, this.key);
+		const data = { 'signal': 'kill' };
+		return Req.postRequest('KillServer', data, ServerID);
+	}
+
+	restartServer(ServerID) {
+		const Req = new Request(this.host, this.key);
+		const data = { 'signal': 'restart' };
+		return Req.postRequest('RestartServer', data, ServerID);
+	}
+
+	sendCommand(ServerID, Command) {
+		const Req = new Request(this.host, this.key);
+		const data = { 'command': Command };
+		return Req.postRequest('SendCommand', data, ServerID);
+	}
+
+	startServer(ServerID) {
+		const Req = new Request(this.host, this.key);
+		const data = { 'signal': 'start' };
+		return Req.postRequest('StartServer', data, ServerID);
+	}
+
+	stopServer(ServerID) {
+		const Req = new Request(this.host, this.key);
+		const data = { 'signal': 'stop' };
+		return Req.postRequest('StopServer', data, ServerID);
+	}
+}
+
 /**
  *
  * @param {String} HOST Host to use
@@ -24,10 +103,10 @@ const sendcommand = require('./methods/sendCommand.js');
  */
 function login(HOST, KEY, callback) {
 	HOST = HOST.trim();
-	if(HOST.endsWith('/')) HOST = HOST.slice(0, -1);
+	if (HOST.endsWith('/')) HOST = HOST.slice(0, -1);
 
-	process.env.CLIENT_NODEACTYL_HOST = HOST;
-	process.env.CLIENT_NODEACTYL_KEY = KEY;
+	// process.env.CLIENT_NODEACTYL_HOST = HOST;
+	// process.env.CLIENT_NODEACTYL_KEY = KEY;
 	axios.get(HOST + '/api/client', {
 		responseEncoding: 'utf8',
 		maxRedirects: 5,
@@ -36,8 +115,8 @@ function login(HOST, KEY, callback) {
 			'Content-Type': 'application/json',
 			'Accept': 'Application/vnd.pterodactyl.v1+json',
 		},
-	}).then(function(response) {
-		if (response.status == 404) {
+	}).then(res => {
+		if (res.status == 404) {
 			callback(false, 'API Key is not valid! (Application)');
 		}
 		else {
@@ -55,13 +134,15 @@ function login(HOST, KEY, callback) {
 
 function fastLogin(HOST, KEY) {
 	HOST = HOST.trim();
-	if(HOST.endsWith('/')) HOST = HOST.slice(0, -1);
-	process.env.CLIENT_NODEACTYL_HOST = HOST;
-	process.env.CLIENT_NODEACTYL_KEY = KEY;
+	if (HOST.endsWith('/')) HOST = HOST.slice(0, -1);
+	// process.env.CLIENT_NODEACTYL_HOST = HOST;
+	// process.env.CLIENT_NODEACTYL_KEY = KEY;
 }
 
 
 module.exports = {
+	Client, 
+
 	login: login,
 	fastLogin: fastLogin,
 
